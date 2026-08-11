d483a2329acc61acaee88d0235d279f26a9036cc
cstainton <carl.stainton@gmail.com>
2026-08-11 14:20:24 +0200

    Put the patch author's name on the files the patch adds
    TeaVM's headers name whoever wrote the file, so the three new files carry mine
    and the two modified ones keep Alexey's. The regenerated patch matches the
    sources that ran in Chrome.

--- teavm-rule-support/src/main/java/org/teavm/classlib/java/util/concurrent/TConcurrentLinkedQueue.java
+++ teavm-rule-support/src/main/java/org/teavm/classlib/java/util/concurrent/TConcurrentLinkedQueue.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2026 Alexey Andreev.
+ *  Copyright 2026 Carl Stainton.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
